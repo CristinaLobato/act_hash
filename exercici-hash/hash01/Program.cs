dab9e5c90db30d120634162683d86a19ecbafa3e
Cristina Lobato <cristinals2283@gmail.com>
2023-10-08 17:31:37 +0200

    Parte tres

--- exercici-hash/hash01/Program.cs
+++ exercici-hash/hash01/Program.cs
@@ -11,51 +11,135 @@ namespace hash01
         static void Main(string[] args)
         {
 
-            String rutaFitcher = null;
-            Console.Write("Introduce la ruta del archivo de texto: ");
-            rutaFitcher = Console.ReadLine();
-         
-            try
-            {
-                if (File.Exists(rutaFitcher))
-                {
-                    // Leer el contenido del archivo
-                    string text = File.ReadAllText(rutaFitcher);
+            Boolean opcion = true;
+            String escoger;
 
-                    // Convertir el texto a un array de bytes
+            do
+            {
+
+                Console.WriteLine("1.- Calcular el HASH del archivo.");
+                Console.WriteLine("2.- Comprovar integridad del archivo");
+                Console.WriteLine("3.- Salir");
+                Console.WriteLine("¿Que deseas hacer?");
+                escoger = Console.ReadLine();
+
+                switch (escoger)
+                {
+                    case "1":
+                        CalcularHash();
+                        break;
+                    case "2":
+                        Integridad();
+                        break;
+                    case "3":
+                        opcion = false;
+                        Console.WriteLine("Hasta pronto.");
+                        break;
+                    default:
+                        Console.WriteLine("Esta opción no está en el menú. Elige una de las tres opciones descritas.");
+                        break;
+                }
+            } while (opcion = true);
+        }
+
+    
+
+
+    static void CalcularHash()
+    {
+
+        try
+        {
+            if (File.Exists(rutaFitcher))
+            {
+                // Leer el contenido del archivo
+                string text = File.ReadAllText(rutaFitcher);
+
+                // Convertir el texto a un array de bytes
+                byte[] bytesIn = Encoding.UTF8.GetBytes(text);
+
+                using (SHA512Managed SHA512 = new SHA512Managed())
+                {
+                    // Calcular el hash
+                    byte[] hashResult = SHA512.ComputeHash(bytesIn);
+
+                    // Convertir el hash a un string hexadecimal
+                    string textoHash = BitConverter.ToString(hashResult).Replace("-", string.Empty);
+
+                    Console.WriteLine($"El hash del archivo es:  {rutaFitcher}:");
+                    Console.WriteLine(textoHash);
+
+                    // Guardar archivo Hash con extensión .SHA:
+                    string rutaSHA = Path.ChangeExtension(rutaFitcher, ".SHA");
+                    File.WriteAllText(rutaSHA, textoHash);
+                    Console.WriteLine($"El hash se ha guardado en el archivo: {rutaSHA}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"¡ERROR! El archivo {rutaFitcher} no existe.");
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"Ha habido un error al leer el archivo: {e.Message}");
+        }
+
+    }
+
+
+    static void Integridad()
+    {
+        Console.WriteLine("Introduce la ruta del archivo: ");
+        String rutaFitcher = Console.ReadLine();
+
+        Console.WriteLine("Introduce la ruta del mismo archivo con extensión .SHA: ");
+        String rutaSHA = Console.ReadLine();
+
+        try
+        {
+            if (File.Exists(rutaFitcher) && File.Exists(rutaSHA))
+            {
+                // Leer el contenido del archivo SHA
+                string textSHA = File.ReadAllText(rutaSHA);
+
+                // Calcular HASH
+                string calcularHash;
+                using (SHA512Managed SHA512 = new SHA512Managed())
+                {
+                    //Leer archivo 
+                    String text = File.ReadAllText(rutaFitcher);
+
+                    // Convertir texto en array y calcular HASH
                     byte[] bytesIn = Encoding.UTF8.GetBytes(text);
 
-                    using (SHA512Managed SHA512 = new SHA512Managed())
-                    {
-                        // Calcular el hash
-                        byte[] hashResult = SHA512.ComputeHash(bytesIn);
+                    byte[] hashResult = SHA512.ComputeHash(bytesIn);
 
-                        // Convertir el hash a un string hexadecimal
-                        string textoHash = BitConverter.ToString(hashResult).Replace("-", string.Empty);
+                    // Convertir el hash a un string hexadecimal
+                    string textoHash = BitConverter.ToString(hashResult).Replace("-", string.Empty);
 
-                        Console.WriteLine($"El hash del archivo es:  {rutaFitcher}:");
-                        Console.WriteLine(textoHash);
+                }
 
-                        // Guardar archivo Hash con extensión .SHA:
-                        string rutaSHA = Path.ChangeExtension(rutaFitcher, ".SHA");
-                        File.WriteAllText(rutaSHA, textoHash);
-                        Console.WriteLine($"El hash se ha guardado en el archivo: {rutaSHA}");
-                    }
+                //Verificar integridad
+
+                if(textoHash == textoSHA)
+                {
+                    Console.WriteLine("La integridad del archivo es correcta ya que el HASH coincide.");
                 }
                 else
                 {
-                    Console.WriteLine($"¡ERROR! El archivo {rutaFitcher} no existe.");
+                    Console.WriteLine("La integridad del archivo NO es correcta ya que el HASH NO coincide.");
                 }
+
             }
-            catch (IOException e)
+            else
             {
-                Console.WriteLine($"Ha habido un error al leer el archivo: {e.Message}");
+                Console.WriteLine($"¡ERROR! El archivo {rutaFitcher} o {rutaSHA} no existe.");
             }
-           
-            Console.ReadKey();
-
-
-            }
-
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"Ha habido un error al leer el archivo: {e.Message}");
         }
     }
+}
